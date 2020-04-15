82c31b6c08a8eed8f9c02953f0ace30a5d0f0a24
Michael Lynch <michael@michaelrlynch.com>
2020-04-15 21:01:47 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,12 +17,11 @@ class ApplicationController < Sinatra::Base
 	end
 
 	post "/signup" do
-		#your code here!
 		user = User.new(:username => params[:username], :password => params[:password])
 		if user.save
-			redirect '/login'
+			redirect "/login"
 		else
-			redirect '/failure'
+			redirect "/failure"
 		end
 	end
 
@@ -31,13 +30,12 @@ class ApplicationController < Sinatra::Base
 	end
 
 	post "/login" do
-		#your code here!
 		user = User.find_by(:username => params[:username])
 		if user && user.authenticate(params[:password])
 			session[:user_id] = user.id
-			redirect '/success'
+			redirect "/success"
 		else
-			redirect '/failure'
+			redirect "failure"
 		end
 	end
 
@@ -60,7 +58,7 @@ class ApplicationController < Sinatra::Base
 
 	helpers do
 		def logged_in?
-			!!session[:iuser_d]
+			!!session[:user_id]
 		end
 
 		def current_user
